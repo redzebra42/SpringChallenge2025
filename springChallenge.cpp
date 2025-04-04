6e860009b3a9c702451cbeb295fa4442f790d7d9
Tristan Delaigue <delaigletristan42@gmail.com>
2025-04-04 08:06:03 +0200

    first commit

---
+++ springChallenge.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int depth;
+    cin >> depth; cin.ignore();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            int value;
+            cin >> value; cin.ignore();
+        }
+    }
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "0" << endl;
+}
